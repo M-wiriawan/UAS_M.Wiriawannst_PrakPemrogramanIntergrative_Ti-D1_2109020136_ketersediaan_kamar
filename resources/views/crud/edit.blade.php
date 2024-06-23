61d1dc44ff61982a3ab779979fa3298393e0d40e
Wiriawan <mwiriawannst@gmail.com>
2024-06-23 23:45:08 +0700

    M. Wiriawan Nst 2109020136

---
+++ resources/views/crud/edit.blade.php
@@ -0,0 +1,50 @@
+@extends('layout.layout')
+
+@section('container')
+
+    <div class="card">
+        <div class="card-header">
+            <h2>Edit Data</h2>
+        </div>
+        <div class="card-body">
+            <form method="POST" action="{{ route('crud.update', $crud->id) }}">
+                @csrf
+                @method('PUT')
+                <div class="row mb-3">
+                    <label for="nama" class="col-sm-2 col-form-label">Nama</label>
+                    <div class="col-sm-4">
+                        <input type="text" class="form-control form-control-sm" id="nama" name="nama" value="{{ $crud->nama }}">
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="pekerjaan" class="col-sm-2 col-form-label">Pekerjaan</label>
+                    <div class="col-sm-4">
+                        <input type="text" class="form-control form-control-sm" id="pekerjaan" name="pekerjaan" value="{{ $crud->pekerjaan }}">
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="alamat" class="col-sm-2 col-form-label">Alamat</label>
+                    <div class="col-sm-4">
+                        <input type="text" class="form-control form-control-sm" id="alamat" name="alamat" value="{{ $crud->alamat }}">
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="email" class="col-sm-2 col-form-label">Email</label>
+                    <div class="col-sm-4">
+                        <input type="email" class="form-control form-control-sm" id="email" name="email" value="{{ $crud->email }}">
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="no_handphone" class="col-sm-2 col-form-label">No Handphone</label>
+                    <div class="col-sm-4">
+                        <input type="text" class="form-control form-control-sm" id="no_handphone" name="no_handphone" value="{{ $crud->no_handphone }}">
+                    </div>
+                </div>
+                <div class="row mb-2">
+                    <label for="" class="col sm-4 col-form-label"></label>
+                    <button type="submit" class="btn btn-primary">Update</button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
